a9ceaa5a0b6e2d20fd02d17373dd6ff86d6683d9
italo <italogithub@gmail.com>
2023-03-02 04:39:13 -0300

    update

--- TicTacGame/src/Game/PlayerStatus/AppPlayer.java
+++ TicTacGame/src/Game/PlayerStatus/AppPlayer.java
@@ -3,17 +3,17 @@ package Game.PlayerStatus;
 import java.util.Scanner;
 
 public class AppPlayer {
-    public Object nickname;
+    public Object originalName;
 
     public static void main() {
 
         Scanner scan = new Scanner(System.in);
         Player player = new Player();
-        System.out.println("Insert your real name: ");
+        System.out.print("Insert your real name: ");
         player.originalName = scan.nextLine();
-        System.out.println("Insert your Nickname: ");
+        System.out.print("Insert your Nickname: ");
         player.nickName = scan.nextLine();
-        System.out.println("Insert your Age: ");
+        System.out.print("Insert your Age: ");
         player.age = scan.nextInt();
         System.out.println("Registration successfully completed.");
 
--- TicTacGame/src/Game/Screen.java
+++ TicTacGame/src/Game/Screen.java
@@ -8,5 +8,12 @@ public class Screen {
         System.out.println(" =                                                                  = ");
         System.out.println(" =                                                                  = ");
         System.out.println(" =                                                                  = ");
+        System.out.println(" = * Created by: Italo Mauricio                                     = ");
+        System.out.println(" = * Student of Information Systems at UFRN                         = ");
+        System.out.println(" =                                                                  = ");
+        System.out.println(" =                                                                  = ");
+        System.out.println(" =                         Since 2023                               = ");
+        System.out.println(" ==================================================================== ");
+        System.out.println();
     }
 }
